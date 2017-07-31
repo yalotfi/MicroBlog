ac38a7d2721abae080d5d0ccbd1f4aea2744613b
Yaseen Lotfi <yalotfi@outlook.com>
2017-07-31 15:16:45 -0400

    init - app structure and environment done

---
+++ README.md
@@ -0,0 +1,9 @@
+# A MicroBlog Platform
+
+Based on an in-depth tutorial to jumpstart Flask web development.
+
+## Supported Features:
+
+1. Homepage for user/blogger
+
+2. Login validation with OpenID
---
+++ config.py
@@ -0,0 +1,9 @@
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'g00n1es_4_lyf3'
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
