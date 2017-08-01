fed3c0897a94d636e0d61b895a50f8bf7885e528
Yaseen Lotfi <yalotfi@outlook.com>
2017-08-01 12:51:24 -0400

    connect db to app

--- config.py
+++ config.py
@@ -1,3 +1,13 @@
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Path to database files
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+# Path to migrated data files
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'g00n1es_4_lyf3'
 
